bae8a237e69807f36de141a785fb59e81563b25f
Eloy Castillo <ejcastillo@banreservas.com>
2024-12-19 10:07:30 -0400

    fix: se agrega conexion string por defecto

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ socketio = SocketIO(app)
 
 # Configuración
 app.secret_key = os.environ.get("FLASK_SECRET_KEY") or "a secret key"
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL") or "postgresql://postgres:123456@localhost/burger-app"
 app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {
     "pool_recycle": 300,
     "pool_pre_ping": True,
